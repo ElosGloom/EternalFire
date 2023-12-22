da8388fbd8cf3926a09574b0e7ca48d6924e3a7b
ElosGloom <elos.gloom@gmail.com>
2023-12-22 16:52:36 +0300

    Tree model, tree cut script

---
+++ Assets/Game/Scripts/DamageOnHit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    // public class DamageOnHit : MonoBehaviour
+    // {
+    //     public int damageAmount;
+    //     public DamageType damageType;
+    //     [SerializeField] private Renderer _renderer;
+    //
+    //     private Dictionary<DamageType, Color> _dictionary = new()
+    //     {
+    //         { DamageType.Pure, Color.white },
+    //         { DamageType.Physical, Color.red },
+    //         { DamageType.Magical, Color.blue },
+    //     };
+    //
+    //     private void Start()
+    //     {
+    //         _renderer.material.color = _dictionary[damageType];
+    //     }
+    //
+    //     private void OnCollisionEnter(Collision other)
+    //     {
+    //         if (other.gameObject.CompareTag("Player"))
+    //         {
+    //             PlayerHealth healthComponent = other.gameObject.GetComponent<PlayerHealth>();
+    //             if (healthComponent == null)
+    //             {
+    //                 return;
+    //             }
+    //
+    //             healthComponent.TakeDamage(damageAmount, damageType);
+    //         }
+    //     }
+    // }
+}
---
+++ Assets/Game/Scripts/TreeCutter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Scripts
+{
+    public class TreeCutter : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private float attackCooldown = 1f;
+        private float _nextAttackTimeLeft;
+
+        private void Start()
+        {
+            _nextAttackTimeLeft = 0;
+        }
+
+        private void Update()
+        {
+            _nextAttackTimeLeft -= Time.deltaTime;
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (_nextAttackTimeLeft < 0)
+            {
+                if (other.gameObject.CompareTag("Tree"))
+                {
+                    animator.SetTrigger("TreeCut");
+                    _nextAttackTimeLeft = attackCooldown;
+                }
+            }
+        }
+    }
+}
