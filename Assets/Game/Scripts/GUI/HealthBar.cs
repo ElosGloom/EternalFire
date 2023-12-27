2f700191a9e35e21d0619b178cbf7bd469b770b7
ElosGloom <elos.gloom@gmail.com>
2023-12-27 22:41:30 +0300

    Bonfire hp bar

---
+++ Assets/Game/Scripts/GUI/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace.UI
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Image hpFill;
+        [SerializeField] private TextMeshProUGUI hpText;
+        [SerializeField] private HealthComponent healthComponent;
+
+        private void Start()
+        {
+            healthComponent.OnHealthChangeEvent += OnHealthChangeEvent;
+        }
+
+        private void OnHealthChangeEvent(float currentHealth, float maxHealth)
+        {
+            hpText.text = $"{currentHealth.ToString("0")}/{maxHealth.ToString()}";
+            hpFill.fillAmount = currentHealth / maxHealth;
+        }
+
+        private void OnDestroy()
+        {
+            healthComponent.OnHealthChangeEvent -= OnHealthChangeEvent;
+        }
+    }
+}
