46942f1e2b32019f9e713b58241fe5286c0fa7c9
ElosGloom <elos.gloom@gmail.com>
2024-01-23 17:14:10 +0300

    new UI, heal and damage vfx logic

---
+++ Assets/Game/Scripts/Health/PlayerFX.cs
@@ -0,0 +1,57 @@
+﻿using FoW;
+using UnityEngine;
+
+namespace Game.Scripts.Health
+{
+    public class PlayerFX : HiderBehaviour
+    {
+        [SerializeField] private HealthComponent healthComponent;
+        [SerializeField] private ParticleSystem damageVFX;
+        [SerializeField] private ParticleSystem healVFX;
+
+
+        private void Start()
+        {
+            healthComponent.DeathEvent += OnDeath;
+            healthComponent.HealthChangeEvent += OnHealthChange;
+        }
+
+        public override void OnVisionStatusChanged(bool isVisible)
+        {
+            if (isVisible)
+            {
+                if (healthComponent.CurrentHealth < healthComponent.MaxHealth)
+                {
+                    healVFX.Play();
+                }
+                damageVFX.Stop();
+                
+            }
+            else
+            {
+                healVFX.Stop();
+                damageVFX.Play();
+            }
+        }
+
+        private void OnHealthChange()
+        {
+           
+            if (healthComponent.CurrentHealth >= healthComponent.MaxHealth)
+            {
+                healVFX.Stop();
+            }
+        }
+
+        private void OnDeath()
+        {
+            damageVFX.Stop();
+        }
+
+        private void OnDestroy()
+        {
+            healthComponent.HealthChangeEvent -= OnHealthChange;
+            healthComponent.DeathEvent -= OnDeath;
+        }
+    }
+}
