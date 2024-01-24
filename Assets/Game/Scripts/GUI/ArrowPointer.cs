20df72803fc82c7c2027bb01fe41b7e5dff2b979
ElosGloom <elos.gloom@gmail.com>
2024-01-24 16:52:07 +0300

    disabled arrow pointer on death

--- Assets/Game/Scripts/GUI/ArrowPointer.cs
+++ Assets/Game/Scripts/GUI/ArrowPointer.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using Game.Scripts.Fire;
+using Game.Scripts.Health;
 using UnityEngine;
 
 namespace Game.Scripts.GUI
 {
     public class ArrowPointer : MonoBehaviour
     {
+        [SerializeField] private HealthComponent playerHealth;
+        
+        private void Start()
+        {
+            playerHealth.DeathEvent += DisableOnDeath;
+        }
+
         private void Update()
         {
             NearestBonfire();
@@ -15,6 +23,16 @@ namespace Game.Scripts.GUI
         {
             var nearestBonfire = FireSystem.Instance.SearchNearestConnectedBonfire(transform.position);
             transform.LookAt(nearestBonfire.transform);
+        }
+
+        private void DisableOnDeath()
+        {
+            gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            playerHealth.DeathEvent -= DisableOnDeath;
         }
     }
 }
