7d3986ea132ab0c7c791f1f0fd4c8a93b25bb20b
ElosGloom <elos.gloom@gmail.com>
2024-02-02 14:42:57 +0300

    foot steps

---
+++ Assets/Game/Scripts/VFXRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class VFXRotation : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _marks;
+    [SerializeField] private Transform _heroTransform;
+
+    private void Update()
+    {
+        UpdateParticleRotation();
+    }
+
+    private void UpdateParticleRotation()
+    {
+        var currentRotation = _heroTransform.eulerAngles.y;
+        currentRotation *= Mathf.Deg2Rad;
+        var particleMain = _marks.main;
+        particleMain.startRotation = currentRotation;
+    }
+}
