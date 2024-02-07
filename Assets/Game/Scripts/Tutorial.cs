5092951786d83f7dd1b4c8b43167409344b7d49c
ElosGloom <elos.gloom@gmail.com>
2024-02-08 01:20:21 +0300

    tutorial

--- Assets/Game/Scripts/Tutorial.cs
+++ Assets/Game/Scripts/Tutorial.cs
@@ -1,19 +1,87 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
 
 namespace Game.Scripts
 {
     public class Tutorial : MonoBehaviour
     {
         [SerializeField] private Transform arrow;
-        [SerializeField] private GameObject tree;
-        [SerializeField] private GameObject activeBonfire;
-        [SerializeField] private GameObject inactiveBonfire;
+        [SerializeField] private Transform tree;
+        [SerializeField] private Transform activeBonfire;
+        [SerializeField] private Transform inactiveBonfire;
+        [SerializeField] private Transform player;
 
-        private void ArrowMove()
+
+        private Transform _currentTarget;
+        private Sequence _arrowAnimation;
+
+        private void Start()
         {
-            
+            _currentTarget = tree;
+            SetArrowTarget(_currentTarget);
         }
-        
-        
+
+        private void Update()
+        {
+            if (_currentTarget != null &&
+                Vector3.Distance(player.position, _currentTarget.position) < 1f)
+            {
+                enabled = false;
+                StartCoroutine(TargetChangeDelayRoutine());
+            }
+        }
+
+        private IEnumerator TargetChangeDelayRoutine()
+        {
+            yield return new WaitForSeconds(2.5f);
+
+            enabled = true;
+            if (_currentTarget == tree)
+            {
+                _currentTarget = activeBonfire;
+            }
+            else if (_currentTarget == activeBonfire)
+            {
+                _currentTarget = inactiveBonfire;
+            }
+            else if (_currentTarget == inactiveBonfire)
+            {
+                _currentTarget = null;
+                SetArrowTarget(null);
+            }
+
+            SetArrowTarget(_currentTarget);
+        }
+
+        private void SetArrowTarget(Transform target)
+        {
+            if (_arrowAnimation != null)
+            {
+                _arrowAnimation.Kill();
+            }
+
+
+            if (target != null)
+            {
+                arrow.gameObject.SetActive(true);
+                var arrowPos = target.position + new Vector3(0, 1.5f, 0);
+                arrow.position = arrowPos;
+
+                _arrowAnimation = DOTween.Sequence();
+                _arrowAnimation.Append(arrow.DOMoveY(arrowPos.y + 2f, 1f).SetEase(Ease.InOutSine));
+                _arrowAnimation.Append(arrow.DOMoveY(arrowPos.y, 1f).SetEase(Ease.InOutSine));
+                _arrowAnimation.SetLoops(-1);
+            }
+            else
+            {
+                arrow.gameObject.SetActive(false);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            DOTween.KillAll();
+        }
     }
 }
